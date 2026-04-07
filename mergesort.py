e6e607504183db1621b3316d1273b0c99e278115
dev <hyrum.rohrbach@gmail.com>
2026-04-07 13:10:38 -0600

    Refactor array builder code into test.py for improved organization and clarity

--- mergesort.py
+++ mergesort.py
@@ -2,20 +2,6 @@
 
 from multiprocessing import Pool
 
-# Array builder/splitter
-# Asks for which .txt file to grab from texts folder
-text_file = input("Enter name of text file: ")
-file = open(f"texts/" + text_file, "r")
-book = file.read()
-# Splits the strings in the .txt file at spaces
-pre_words = book.split(" ")
-# Removes punctuation from the front and end of strings, cleaning up the words
-word_array = []
-for word in pre_words:
-    cleaned = word.strip('"!?.()')
-    word_array.append(cleaned)
-
-print(word_array)
 
 
 
